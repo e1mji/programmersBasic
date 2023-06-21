77bec3e002fec1f15dead7d227ffee5c5715161d
eam ji hun <133019246+e1mji@users.noreply.github.com>
2023-06-21 21:35:21 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.02 ms, Memory: 74.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.java
@@ -0,0 +1,15 @@
+class Solution {
+  	static int[] solution(int[] num_list) {
+        int evenCount = 0;
+        int oddCount = 0;
+        for(int i = 0; i<num_list.length; i++) {
+        	if(num_list[i] %2 ==0) {
+        		evenCount++;
+        	} else {
+        		oddCount++;
+        	}
+        }
+        int[] answer = new int[] {evenCount, oddCount};
+        return answer;
+    }
+}
