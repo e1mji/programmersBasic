6a49e2eed518da42ac2d8597298c10f769127c0b
eam ji hun <133019246+e1mji@users.noreply.github.com>
2023-06-27 00:55:44 +0900

    [level 0] Title: 피자 나눠 먹기 (2), Time: 0.02 ms, Memory: 76.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.java
@@ -0,0 +1,10 @@
+class Solution {
+  	static int solution(int n) {
+        int answer = 1;
+        
+        while( (6 * answer)%n != 0 ){
+            answer++;            
+        }
+        return answer;
+	}
+}
