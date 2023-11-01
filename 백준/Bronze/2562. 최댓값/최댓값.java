e5710178457edc9e850cc5273d032c60afaf7834
eam ji hun <133019246+e1mji@users.noreply.github.com>
2023-11-01 13:54:00 +0900

    [Bronze III] Title: 최댓값, Time: 204 ms, Memory: 17684 KB -BaekjoonHub

---
+++ 백준/Bronze/2562. 최댓값/최댓값.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		int[] arr = new int [9];
+		int max = 0;
+		int number = 0;
+		Scanner sc = new Scanner(System.in);
+		
+		for(int i = 0; i < arr.length; i++) {
+			arr[i] = sc.nextInt();
+		}
+		
+		for(int i = 0; i < arr.length; i++) {
+			if(arr[i] > max) {
+				max = arr[i];
+				number = i + 1;
+			}
+		}
+		
+		System.out.println(max);
+		System.out.println(number);
+
+	}
+
+}
